3b7d8cecdb7d101718386dd11e7b8328085d0df7
priyanshkhas <priyanshkhasdev@gmail.com>
2025-08-24 15:11:16 +0530

    add register controller and bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,36 @@
+// import mongoose from "mongoose";
+// import {DB_NAME } from "../constants.js";
+
+// const connectDB = async () => {
+    
+//    try {
+//       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
+//    } catch (error){
+//       console.log("MONGODB connection error" , error);
+//       process.exit(1)
+//    }
+
+// }
+
+// export default connectDB
+
+
+
+
 import mongoose from "mongoose";
-import {DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    
-   try {
-      const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} `);
-   } catch (error){
-      console.log("MONGODB connection error" , error);
-      process.exit(1)
-   }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
 
-}
-
-export default connectDB
+export default connectDB;
